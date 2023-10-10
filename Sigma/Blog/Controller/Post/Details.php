d03a0cfbeb73ed0faab1b293b72ac2f9a372324c
Rohan Dhakad <rohan.dhakad@sigmainfo.net>
2023-10-11 01:12:48 +0530

    creating object through D.I. in a new way using php8.1

--- Sigma/Blog/Controller/Post/Details.php
+++ Sigma/Blog/Controller/Post/Details.php
@@ -4,19 +4,28 @@ namespace Sigma\Blog\Controller\Post;
 
 use Magento\Customer\Model\Session;
 use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\App\ObjectManager;
 
 class Details implements HttpGetActionInterface
 {
+
+    public function __construct(
+        private Session $session
+    ) {}
+
     public function execute()
     {
-        //it is not good practice to create instance of a class using ObjectManager, it is just for knowledge
-        $object = ObjectManager::getInstance();
-        //getting data of current logged-in customer
-        $session = $object->get(Session::class);
         echo '<pre>';
-        var_dump($session->getData());
+        var_dump($this->session->getData());
         die();
-        // die("Blog post list");
     }
 }
+
+
+//  //it is not good practice to create instance of a class using ObjectManager, it is just for knowledge
+//         $object = ObjectManager::getInstance();
+//         //getting data of current logged-in customer
+//         $session = $object->get(Session::class);
+//         echo '<pre>';
+//         var_dump($session->getData());
+//         die();
+//         // die("Blog post list");
